82fea54b0bb8f4e4a6badf1ecfe3496a2fa8f60b
samcooper <sam.o.cooper@gmail.com>
2017-04-29 14:07:40 +0100

    Formatting

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['nuclitrack'],
     install_requires=['Cython','numpy','matplotlib','scipy','scikit-image','scikit-learn','pygame','kivy','h5py'],
     ext_modules=[
-        Extension("tracking_c_tools", ["pyx_files/tracking_c_tools.c"], include_dirs=[numpy.get_include()]),
-        Extension("segmentation_c_tools", ["pyx_files/segmentation_c_tools.c"], include_dirs=[numpy.get_include()]),
-        Extension("numpy_to_image", ["pyx_files/numpy_to_image.c"], include_dirs=[numpy.get_include()])]
+        Extension("tracking_c_tools", ["nuclitrack/tracking_c_tools.c"], include_dirs=[numpy.get_include()]),
+        Extension("segmentation_c_tools", ["nuclitrack/segmentation_c_tools.c"], include_dirs=[numpy.get_include()]),
+        Extension("numpy_to_image", ["nuclitrack/numpy_to_image.c"], include_dirs=[numpy.get_include()])]
 )
