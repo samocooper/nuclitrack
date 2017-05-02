a727003196b5297d0fca6037846f4f6c0686b53e
samcooper <sam.o.cooper@gmail.com>
2017-05-02 20:05:06 +0100

    Splitting ui from functions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy
 
 setup(
     name='nuclitrack',
-    version='1.1.4',
+    version='1.1.6',
     description='Nuclei tracking program',
     author='Sam Cooper',
     author_email='sam@socooper.com',
--- setup_pyx.py
+++ setup_pyx.py
@@ -12,7 +12,7 @@ setup(
     packages=['nuclitrack'],
     install_requires=['Cython','numpy','matplotlib','scipy','scikit-image','scikit-learn','pygame','kivy','h5py'],
     ext_modules=cythonize([
-        Extension("ctooltracking", ["nuclitrack/ctooltracking.c"], include_dirs=[numpy.get_include()]),
-        Extension("ctoolsegmentation", ["nuclitrack/ctoolsegmentation.c"], include_dirs=[numpy.get_include()]),
-        Extension("numpytoimage", ["nuclitrack/numpytoimage.c"], include_dirs=[numpy.get_include()])])
+        Extension("ctooltracking", ["pyx_files/ctooltracking.pyx"], include_dirs=[numpy.get_include()]),
+        Extension("ctoolsegmentation", ["pyx_files/ctoolsegmentation.pyx"], include_dirs=[numpy.get_include()]),
+        Extension("numpytoimage", ["pyx_files/numpytoimage.pyx"], include_dirs=[numpy.get_include()])])
 )
