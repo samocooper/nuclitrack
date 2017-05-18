59cc6feb2cf8b6ee1f91ede88688299f20999ceb
samocooper <sam.o.cooper@gmail.com>
2017-05-18 14:50:53 +0100

    Proper error handling

--- nuclitrack/batchanalyse.py
+++ nuclitrack/batchanalyse.py
@@ -10,7 +10,7 @@ from . import classifycells
 from . import trackcells
 from .segmentimages import segment_image
 
-def batchanalyse(text_file, param_file, output_file):
+def batchanalyse(text_file, param_file, output_file, ring_flag=False):
 
     print('Loading Images')
     fov = h5py.File(output_file + '.hdf5', "a")
@@ -42,10 +42,10 @@ def batchanalyse(text_file, param_file, output_file):
             labels[i, :, :] = labels_list[i]
 
     print('Extracting features')
-    feature_num = 20 + 3 * (channel_num - 1) + 1
-
+    features = dict()
+    features['tracking'] = np.zeros((1, 13))
+    features['data'] = np.zeros((1, 22))
     counter = 1
-    features = np.zeros((1, feature_num))
 
     for i in range(frames):
 
@@ -53,17 +53,19 @@ def batchanalyse(text_file, param_file, output_file):
         for j in range(channel_num):
             files.append(file_list[j][i])
 
-        feature_mat, labels[i, :, :], counter = extractfeatures.framefeatures(files, labels[i, :, :],
-                                                                              feature_num, counter)
-        feature_mat[:, 1] = i
-        features = np.vstack((features, feature_mat))
+        temp_feat, labels[i, :, :], counter = extractfeatures.framefeatures(files, labels[i, :, :], counter, ring_flag)
+        temp_feat['tracking'][:, 1] = i
+        features['tracking'] = np.vstack((features['tracking'], temp_feat['tracking']))
+        features['data'] = np.vstack((features['data'], temp_feat['data']))
 
-    features[1:, 17:19] = 1
-    features = features[np.argsort(features[:, 0]), :]
+    inds = np.argsort(features['tracking'][:, 0])
+    features['tracking'] = features['tracking'][inds, :]
+    features['data'] = features['data'][inds, :]
+    features['tracking'][1:, 5] = 1.
 
-    features = classifycells.classifycells(features, params['training_data'][...])
-
-    tracking_object = trackcells.TrackCells(features, frames, params['track_param'][...])
+    features = classifycells.classifycells(features, params['training'])
+    tracking_object = trackcells.TrackCells(features=features['tracking'][...],
+                                            track_param=params['track_param'][...], frames=frames)
 
     print('Tracking cells')
     counter = 0
@@ -79,14 +81,19 @@ def batchanalyse(text_file, param_file, output_file):
             counter += 1
             print(counter)
 
-    tracks, features, count, double = tracking_object.get()
+    print('saving data')
+
+    tracks, features['tracking'][...], count, double = tracking_object.get()
     tracks_stored = np.zeros(int(max(tracks[:, 4])))
 
+    features_hdf5 = fov.create_group('features')
+    features_hdf5.create_dataset("tracking", data=features['tracking'])
+    features_hdf5.create_dataset("data", data=features['data'])
     fov.create_dataset("labels", data=labels)
-    fov.create_dataset("features", data=features)
     fov.create_dataset("tracks", data=tracks)
     fov.create_dataset("tracks_stored", data=tracks_stored)
 
     trackcells.save_csv(features, tracks, output_file + '.csv')
 
+    print('done')
 
