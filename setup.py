639278dab198f58504321d1da6ca63f142f68efb
samocooper <sam.o.cooper@gmail.com>
2017-08-21 11:26:06 +0100

    Restructuring

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     license='MIT',
     packages=['nuclitrack'],
     ext_modules=[
-        Extension("ctooltracking", ["nuclitrack/ctooltracking.c"], include_dirs=[numpy.get_include()]),
-        Extension("classifyim", ["pyx_files/classifyim.c"], include_dirs=[numpy.get_include()]),
-        Extension("ctoolsegmentation", ["nuclitrack/ctoolsegmentation.c"], include_dirs=[numpy.get_include()]),
-        Extension("numpytoimage", ["nuclitrack/numpytoimage.c"], include_dirs=[numpy.get_include()])]
+        Extension("ctooltracking", ["nuclitrack/nuclitrack_ctools/ctooltracking.c"], include_dirs=[numpy.get_include()]),
+        Extension("classifyim", ["nuclitrack/nuclitrack_ctools/classifyim.c"], include_dirs=[numpy.get_include()]),
+        Extension("ctoolsegmentation", ["nuclitrack/nuclitrack_ctools/ctoolsegmentation.c"], include_dirs=[numpy.get_include()]),
+        Extension("numpytoimage", ["nuclitrack/nuclitrack_ctools/numpytoimage.c"], include_dirs=[numpy.get_include()])]
 )
