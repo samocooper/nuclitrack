897f8e7ac4f4d22da11750e1877a94449da8ec7c
samcooper <sam.o.cooper@gmail.com>
2017-08-22 12:00:59 +0100

    changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy
 
 setup(
     name='nuclitrack',
-    version='2.03',
+    version='2.0.4',
     description='Nuclei tracking program',
     author='Sam Cooper',
     author_email='sam@socooper.com',
